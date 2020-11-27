c8693457c3dc90440adec01663f920a71b369126
SadiaSanam <syasanam@gmail.com>
2020-11-27 13:32:23 -0500

    updated date and added loader

---
+++ src/FormatDate.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+export default function FormatDate(props) {
+let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[props.date.getDay()];
+  let hours = props.date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  
+  let minutes = props.date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+let monthIndex = props.date.getMonth();
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+];
+let date = props.date.getDate();
+let year = props.date.getFullYear();
+
+  return (
+    <div>
+    {months[monthIndex]}, {date}, {year}`
+    <br/>
+    {day} {hours}:{minutes}
+    </div>
+  );
+}
--- src/Weather.js
+++ src/Weather.js
@@ -1,40 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
+import Loader from 'react-loader-spinner'
+import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-    return (<div className="weather">
-    <form>
-    <div className="row">
-     <div className="col-9">
-      <input type="search"
-       placeholder="Enter City Name"
-       className="form-control"
-       />
-    </div>
-     <div>
-        <input type="submit"
-        value="Search"
-        className="btn btn-primary"/>
-     </div>
-    </div>
-    </form>
-        <h1>New York</h1>
-        <ul>
-            <li>Thursday 7:00 PM</li>
-            <li>Cloudy</li>
-        </ul>
-        <div className="row">
-            <div className="col-6">
-            <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="Cloudy"/> 11°C
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      icon: response.data.weather[0].icon,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    const apiKey = "0a20e0390f583da09083f85d22ebb0d5";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
             </div>
-            <div className="col-6">
-                <ul>
-                    <li>Precipitation:2%</li>
-                    <li>Humidity:75%</li>
-                    <li>Wind:6km/h</li>
-                </ul>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
             </div>
-        </div>
-    </div>
-   );
+          </div>
+        </form>
+        <WeatherInfo data={weatherData} />
+        <WeatherForecast city={weatherData.city} />
+      </div>
+    );
+  } else {
+    search();
+    return (
+    <Loader
+         type="Puff"
+         color="#00BFFF"
+         height={100}
+         width={100}
+         timeout={3000}
+       />);
+  }
 }
